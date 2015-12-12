fa1236a9b26fb7821d8009f622b2803500a1fddb
Maxwolf <ron.mcdowell@gmail.com>
2015-12-11 21:30:51 -0800

    Renamed TrailGame to TrailConsole since it is becoming the console only example of the trail simulation. Later I intend to construct a WinForms version that I can use to test the full disconnection from simulation and presentation of it to user.

--- Src/TrailConsole/Program.cs
+++ Src/TrailConsole/Program.cs
@@ -2,10 +2,10 @@
 using System.Threading;
 using TrailSimulation.Game;
 
-namespace TrailGame
+namespace TrailConsole
 {
     /// <summary>
-    ///     Trail Simulation Game
+    ///     Trail Simulation Game - Console Edition
     /// </summary>
     internal static class Program
     {
