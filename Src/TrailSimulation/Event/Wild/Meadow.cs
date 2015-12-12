37ffd3e9ca515a0b7f68681b042df4babc04a9b3
Maxwolf <ron.mcdowell@gmail.com>
2015-12-12 06:49:34 -0800

    Added the last batch of random event stubs, these was for wild and whacky events that can occur that don't fit other categories. Examples would include the gravesite of another dead player but that is not a random one, those would be strangers, abandoned vehicle, find berried, thief comes, etc.

---
+++ Src/TrailSimulation/Event/Wild/Meadow.cs
@@ -0,0 +1,42 @@
+﻿using TrailSimulation.Entity;
+using TrailSimulation.Game;
+
+namespace TrailSimulation.Event
+{
+    [DirectorEvent(EventCategory.Wild)]
+    public sealed class Meadow : EventProduct
+    {
+        /// <summary>
+        ///     Creates a new instance of an event product with the specified event type for reference purposes.
+        /// </summary>
+        /// <param name="category">
+        ///     what type of event this will be, used for grouping and filtering and triggering events by type rather than type of.
+        /// </param>
+        public Meadow(EventCategory category) : base(category)
+        {
+        }
+
+        /// <summary>
+        ///     Fired when the event handler associated with this enum type triggers action on target entity. Implementation is
+        ///     left completely up to handler.
+        /// </summary>
+        /// <param name="sourceEntity">
+        ///     Entities which the event is going to directly affect. This way there is no confusion about
+        ///     what entity the event is for. Will require casting to correct instance type from interface instance.
+        /// </param>
+        public override void Execute(IEntity sourceEntity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        ///     Fired when the simulation would like to render the event, typically this is done AFTER executing it but this could
+        ///     change depending on requirements of the implementation.
+        /// </summary>
+        /// <returns>Text user interface string that can be used to explain what the event did when executed.</returns>
+        protected override string OnRender()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
