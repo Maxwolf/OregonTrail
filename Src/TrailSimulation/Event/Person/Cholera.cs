9575a6f6184e8babd5f1a741372472ec10318135
Maxwolf <ron.mcdowell@gmail.com>
2015-12-18 03:25:22 -0800

    Added event for cholera infection.

--- Src/TrailSimulation/Event/Person/Cholera.cs
+++ Src/TrailSimulation/Event/Person/Cholera.cs
@@ -1,34 +1,23 @@
-﻿using System;
-using TrailSimulation.Entity;
+﻿using TrailSimulation.Entity;
 using TrailSimulation.Game;
 
 namespace TrailSimulation.Event
 {
+    /// <summary>
+    ///     Cholera is an infection of the small intestine by some strains of the bacterium Vibrio cholerae. Symptoms may range
+    ///     from none, to mild, to severe.
+    /// </summary>
     [DirectorEvent(EventCategory.Person)]
-    public sealed class Cholera : EventProduct
+    public sealed class Cholera : EventPersonInfect
     {
         /// <summary>
-        ///     Fired when the event handler associated with this enum type triggers action on target entity. Implementation is
-        ///     left completely up to handler.
+        ///     Fired after the event has executed and the infection flag set on the person.
         /// </summary>
-        /// <param name="sourceEntity">
-        ///     Entities which the event is going to directly affect. This way there is no confusion about
-        ///     what entity the event is for. Will require casting to correct instance type from interface instance.
-        /// </param>
-        public override void Execute(IEntity sourceEntity)
+        /// <param name="person">Person whom is now infected by whatever you say they are here.</param>
+        /// <returns>Name or type of infection the person is currently affected with.</returns>
+        protected override string OnPostInfection(Person person)
         {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        ///     Fired when the simulation would like to render the event, typically this is done AFTER executing it but this could
-        ///     change depending on requirements of the implementation.
-        /// </summary>
-        /// <param name="sourceEntity"></param>
-        /// <returns>Text user interface string that can be used to explain what the event did when executed.</returns>
-        protected override string OnRender(IEntity sourceEntity)
-        {
-            throw new NotImplementedException();
+            return $"{person.Name} has cholera.";
         }
     }
 }
