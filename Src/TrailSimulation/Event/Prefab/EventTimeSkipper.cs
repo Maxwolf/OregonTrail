08c1ac9b911612859859d9f0d831311888a453c6
Maxwolf <ron.mcdowell@gmail.com>
2015-12-17 21:34:12 -0800

    Created a new class that will deal with skipping set intervals of time in the form of days. Will be used by events that need to skip over sections of time and also still simulate people and events that will come from doing that. Still need to decide if I should make a copy of the resting state and modify it or make it so existing rest state works with it.. probably the former since I don't want to mess with the logic since so much has already been built up around it (and I like it).

---
+++ Src/TrailSimulation/Event/Prefab/EventTimeSkipper.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using TrailSimulation.Entity;
+using TrailSimulation.Game;
+
+namespace TrailSimulation.Event
+{
+    /// <summary>
+    ///     Prefab for an event that will be able to skip time up to a set amount of days. The purpose of which is to
+    ///     accommodate events which force the player to lose time. When this happens the simulation ticks each of those days,
+    ///     allowing any events such as people starving to death and wild animals occur as they normally would.
+    /// </summary>
+    public abstract class EventTimeSkipper : EventProduct
+    {
+        /// <summary>
+        ///     String builder that will hold all the data from event execution.
+        /// </summary>
+        private StringBuilder _eventText;
+
+        /// <summary>
+        ///     Creates a new instance of an event product with the specified event type for reference purposes.
+        /// </summary>
+        /// <param name="category">
+        ///     what type of event this will be, used for grouping and filtering and triggering events by type rather than type of.
+        /// </param>
+        protected EventTimeSkipper(EventCategory category) : base(category)
+        {
+            // Create the string builder that will hold representation of event action to display for debugging.
+            _eventText = new StringBuilder();
+        }
+
+        /// <summary>
+        ///     Fired when the event handler associated with this enum type triggers action on target entity. Implementation is
+        ///     left completely up to handler.
+        /// </summary>
+        /// <param name="sourceEntity">
+        ///     Entities which the event is going to directly affect. This way there is no confusion about
+        ///     what entity the event is for. Will require casting to correct instance type from interface instance.
+        /// </param>
+        public override void Execute(IEntity sourceEntity)
+        {
+            // Clear out the text from the string builder.
+            _eventText.Clear();
+
+
+        }
+
+        /// <summary>
+        ///     Fired when the simulation would like to render the event, typically this is done AFTER executing it but this could
+        ///     change depending on requirements of the implementation.
+        /// </summary>
+        /// <param name="sourceEntity"></param>
+        /// <returns>Text user interface string that can be used to explain what the event did when executed.</returns>
+        protected override string OnRender(IEntity sourceEntity)
+        {
+            return _eventText.ToString();
+        }
+
+        /// <summary>
+        ///     Fired when the event is closed by the user or system after being executed and rendered out on text user interface.
+        /// </summary>
+        public override void OnEventClose()
+        {
+            base.OnEventClose();
+
+
+        }
+    }
+}
