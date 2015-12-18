3f1cb4ded4367ddde6893d7340c3b934c064031d
Maxwolf <ron.mcdowell@gmail.com>
2015-12-17 20:44:39 -0800

    Created event for a daring thief that will steal things from the vehicle inventory, and even kill if necessary to get what he wants.

--- Src/TrailSimulation/Event/Wild/Thief.cs
+++ Src/TrailSimulation/Event/Wild/Thief.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Text;
 using TrailSimulation.Entity;
 using TrailSimulation.Game;
 
 namespace TrailSimulation.Event
 {
+    /// <summary>
+    ///     Robber who can come in the middle of the night and steal things from the vehicle inventory. He is also very
+    ///     dangerous and will do whatever it takes to get what he wants, so there is a chance some of your party members may
+    ///     get murdered.
+    /// </summary>
     [DirectorEvent(EventCategory.Wild)]
-    public sealed class Thief : EventProduct
+    public sealed class Thief : EventItemDestroyer
     {
         /// <summary>
         ///     Creates a new instance of an event product with the specified event type for reference purposes.
@@ -18,27 +24,56 @@ namespace TrailSimulation.Event
         }
 
         /// <summary>
-        ///     Fired when the event handler associated with this enum type triggers action on target entity. Implementation is
-        ///     left completely up to handler.
+        ///     Fired by the item destroyer event prefab before items are destroyed.
         /// </summary>
-        /// <param name="sourceEntity">
-        ///     Entities which the event is going to directly affect. This way there is no confusion about
-        ///     what entity the event is for. Will require casting to correct instance type from interface instance.
-        /// </param>
-        public override void Execute(IEntity sourceEntity)
+        /// <param name="destroyedItems">Items that were destroyed from the players inventory.</param>
+        protected override string OnPostDestroyItems(IDictionary<Entities, int> destroyedItems)
         {
-            throw new NotImplementedException();
+            // Grab an instance of the game simulation.
+            var game = GameSimulationApp.Instance;
+
+            // Change event text depending on if items were destroyed or not.
+            var postDestroy = new StringBuilder();
+            if (destroyedItems.Count > 0)
+            {
+                // Lists out the people and items destroyed.
+                postDestroy.AppendLine("the loss of:");
+
+                // Check if there are enough clothes to keep people warm, need two sets of clothes for every person.
+                if (game.Vehicle.Inventory[Entities.Clothes].Quantity >= (game.Vehicle.PassengerLivingCount*2))
+                    return postDestroy.ToString();
+
+                // Attempts to kill the living passengers of the vehicle.
+                var murderedPassengers = game.Vehicle.Passengers.TryKill();
+
+                // If the killed passenger list contains any entries we print them out.
+                foreach (var person in murderedPassengers)
+                {
+                    // Only proceed if person is actually dead.
+                    if (person.HealthLevel == HealthLevel.Dead)
+                        postDestroy.AppendLine($"{person.Name} (murdered)");
+                }
+            }
+            else
+            {
+                // Got lucky nothing was destroyed!
+                postDestroy.AppendLine("no loss of items.");
+            }
+
+            // Returns the processed text to event for rendering.
+            return postDestroy.ToString();
         }
 
         /// <summary>
-        ///     Fired when the simulation would like to render the event, typically this is done AFTER executing it but this could
-        ///     change depending on requirements of the implementation.
+        ///     Fired by the item destroyer event prefab after items are destroyed.
         /// </summary>
-        /// <param name="sourceEntity"></param>
-        /// <returns>Text user interface string that can be used to explain what the event did when executed.</returns>
-        protected override string OnRender(IEntity sourceEntity)
+        protected override string OnPreDestroyItems()
         {
-            throw new NotImplementedException();
+            var theifPrompt = new StringBuilder();
+            theifPrompt.Clear();
+            theifPrompt.AppendLine("Thief comes in the");
+            theifPrompt.Append("night resulting in ");
+            return theifPrompt.ToString();
         }
     }
 }
