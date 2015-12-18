087415fec8e9098aaf947b7f81c30dd0f8d17535
Maxwolf <ron.mcdowell@gmail.com>
2015-12-18 03:51:44 -0800

    Added event for suffering exhaustion injury.

--- Src/TrailSimulation/Event/Person/SufferingExhaustion.cs
+++ Src/TrailSimulation/Event/Person/SufferingExhaustion.cs
@@ -1,34 +1,22 @@
-﻿using System;
-using TrailSimulation.Entity;
+﻿using TrailSimulation.Entity;
 using TrailSimulation.Game;
 
 namespace TrailSimulation.Event
 {
+    /// <summary>
+    ///     A person with weakness or fatigue lacks energy, feels weary, and is constantly tired.
+    /// </summary>
     [DirectorEvent(EventCategory.Person)]
-    public sealed class SufferingExhaustion : EventProduct
+    public sealed class SufferingExhaustion : EventPersonInjure
     {
         /// <summary>
-        ///     Fired when the event handler associated with this enum type triggers action on target entity. Implementation is
-        ///     left completely up to handler.
+        ///     Fired after the event has executed and the injury flag set on the person.
         /// </summary>
-        /// <param name="sourceEntity">
-        ///     Entities which the event is going to directly affect. This way there is no confusion about
-        ///     what entity the event is for. Will require casting to correct instance type from interface instance.
-        /// </param>
-        public override void Execute(IEntity sourceEntity)
+        /// <param name="person">Person whom is now injured by whatever you say they are here.</param>
+        /// <returns>Describes what type of physical injury has come to the person.</returns>
+        protected override string OnPostInjury(Person person)
         {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        ///     Fired when the simulation would like to render the event, typically this is done AFTER executing it but this could
-        ///     change depending on requirements of the implementation.
-        /// </summary>
-        /// <param name="sourceEntity"></param>
-        /// <returns>Text user interface string that can be used to explain what the event did when executed.</returns>
-        protected override string OnRender(IEntity sourceEntity)
-        {
-            throw new NotImplementedException();
+            return $"{person.Name} is suffering from exhaustion.";
         }
     }
 }
