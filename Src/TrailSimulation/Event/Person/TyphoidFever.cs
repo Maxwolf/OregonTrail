f6f2e2c1d8bb4b3290b5fc7e4b0f800128f3e94c
Maxwolf <ron.mcdowell@gmail.com>
2015-12-19 03:48:47 -0800

    Added event for typhoid fever.

--- Src/TrailSimulation/Event/Person/TyphoidFever.cs
+++ Src/TrailSimulation/Event/Person/TyphoidFever.cs
@@ -1,36 +1,25 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using TrailSimulation.Entity;
 using TrailSimulation.Game;
 
 namespace TrailSimulation.Event
 {
+    /// <summary>
+    ///     An infectious bacterial fever with an eruption of red spots on the chest and abdomen and severe intestinal
+    ///     irritation.
+    /// </summary>
     [DirectorEvent(EventCategory.Person)]
     [SuppressMessage("ReSharper", "UnusedMember.Global")]
-    public sealed class TyphoidFever : EventProduct
+    public sealed class TyphoidFever : EventPersonInfect
     {
         /// <summary>
-        ///     Fired when the event handler associated with this enum type triggers action on target entity. Implementation is
-        ///     left completely up to handler.
+        ///     Fired after the event has executed and the infection flag set on the person.
         /// </summary>
-        /// <param name="sourceEntity">
-        ///     Entities which the event is going to directly affect. This way there is no confusion about
-        ///     what entity the event is for. Will require casting to correct instance type from interface instance.
-        /// </param>
-        public override void Execute(IEntity sourceEntity)
+        /// <param name="person">Person whom is now infected by whatever you say they are here.</param>
+        /// <returns>Name or type of infection the person is currently affected with.</returns>
+        protected override string OnPostInfection(Person person)
         {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        ///     Fired when the simulation would like to render the event, typically this is done AFTER executing it but this could
-        ///     change depending on requirements of the implementation.
-        /// </summary>
-        /// <param name="sourceEntity"></param>
-        /// <returns>Text user interface string that can be used to explain what the event did when executed.</returns>
-        protected override string OnRender(IEntity sourceEntity)
-        {
-            throw new NotImplementedException();
+            return $"{person.Name} has typhoid fever.";
         }
     }
 }
