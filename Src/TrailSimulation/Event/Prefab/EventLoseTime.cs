212ae3d448e6e9a52d253d789e134f3938873703
Maxwolf <ron.mcdowell@gmail.com>
2015-12-19 23:09:30 -0800

    Added override for OnEventClose() to the lose time event. It will now check if the random event window is attached and then grab the instance of it, once this is done it will set the form to be the event skip day one. The goal here is to make the normal event be able to spawn a new form on the parent below it without really having any clue how that works.

--- Src/TrailSimulation/Event/Prefab/EventLoseTime.cs
+++ Src/TrailSimulation/Event/Prefab/EventLoseTime.cs
@@ -33,5 +33,31 @@ namespace TrailSimulation.Event
         {
             throw new NotImplementedException();
         }
+
+        /// <summary>
+        ///     Fired when the event is closed by the user or system after being executed and rendered out on text user interface.
+        /// </summary>
+        public override void OnEventClose()
+        {
+            base.OnEventClose();
+
+            // Grab the game simulation instance into smaller variable.
+            var game = GameSimulationApp.Instance;
+
+            // Check if the window manager has the random event window attached (it should be).
+            if (!game.WindowManager.ContainsWindow(GameWindow.RandomEvent))
+                return;
+
+            // Cast the grabbed window into the random event window.
+            var randomWindow = game.WindowManager.Windows[GameWindow.RandomEvent] as RandomEvent;
+
+            // Complain if the window is null.
+            if (randomWindow == null)
+                throw new InvalidCastException(
+                    "Unable to cast window manager random event window to random event window type!");
+
+            // Set the form of the random event window to the event for skipping days.
+            randomWindow.SetForm(typeof (EventSkipDay));
+        }
     }
 }
