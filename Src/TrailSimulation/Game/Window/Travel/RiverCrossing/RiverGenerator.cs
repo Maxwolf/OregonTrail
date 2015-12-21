6030f26e3b56164f7bf58076085f490ffbade773
Maxwolf <ron.mcdowell@gmail.com>
2015-12-21 00:42:04 -0800

    Added new river option enumeration being used on location to the river generator used by the river crossing form in the travel window. There is a switch statement now the river option, depending on value we will change up the user data properties to fit the required scenario before letting the player decide how they would like to proceed.

--- Src/TrailSimulation/Game/Window/Travel/RiverCrossing/RiverGenerator.cs
+++ Src/TrailSimulation/Game/Window/Travel/RiverCrossing/RiverGenerator.cs
@@ -1,4 +1,6 @@
-﻿namespace TrailSimulation.Game
+﻿using System;
+
+namespace TrailSimulation.Game
 {
     /// <summary>
     ///     Special data class that is used to generate river data in the travel info as requested. Creation of this object
@@ -13,8 +15,33 @@
         {
             // Randomly generates statistics about the river each time you cross it.
             RiverDepth = GameSimulationApp.Instance.Random.Next(1, 20);
-            FerryCost = GameSimulationApp.Instance.Random.Next(3, 8);
-            FerryDelayInDays = GameSimulationApp.Instance.Random.Next(1, 10);
+
+            // Only setup ferry cost and delay if this is that type of crossing.
+            switch (GameSimulationApp.Instance.Trail.CurrentLocation.RiverCrossOption)
+            {
+                case RiverOption.FerryOperator:
+                    // Ferry operator will ask the player to wait some days, and pay money for the privilege to ride his ferry.
+                    FerryCost = GameSimulationApp.Instance.Random.Next(3, 8);
+                    FerryDelayInDays = GameSimulationApp.Instance.Random.Next(1, 10);
+                    break;
+                case RiverOption.ForkAndFord:
+                    // No special options present, will have to ford or float vehicle over the rover.
+                    FerryCost = 0;
+                    FerryDelayInDays = 0;
+                    break;
+                case RiverOption.IndianGuide:
+                    // Indian guide will ask you for sets of clothing, more sets the more animals killed in hunting.
+                    FerryCost = 0;
+                    FerryDelayInDays = 0;
+                    break;
+                case RiverOption.None:
+                    throw new ArgumentException(
+                        "Unable to generate river without having options configured to some value other than NONE!");
+                default:
+                    throw new ArgumentException(
+                        "Unable to figure out what the river option value should be! Check value being sent to river generator class!");
+            }
+
             RiverWidth = GameSimulationApp.Instance.Random.Next(100, 1500);
             CrossingType = RiverCrossChoice.None;
         }
