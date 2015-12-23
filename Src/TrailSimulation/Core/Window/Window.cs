9e6f7af3a2da69208b8d77958ec1a78fe3d34970
Maxwolf <ron.mcdowell@gmail.com>
2015-12-23 04:09:57 -0800

    Rebuilt the base window class to support dynamic mappings for added menu commands. Now it doesn't matter which number or order the commands are shown they will always match up to a incremented number related to a refresh operation that clears and then calculates the mappings before each render request.

--- Src/TrailSimulation/Core/Window/Window.cs
+++ Src/TrailSimulation/Core/Window/Window.cs
@@ -2,11 +2,6 @@
 // <copyright file="Window.cs" company="Ron 'Maxwolf' McDowell">
 //   ron.mcdowell@gmail.com
 // </copyright>
-// <summary>
-//   Facilitates the ability to control the entire simulation with the passes interface reference. Server simulation
-//   keeps track of all currently loaded game modes and will only tick the top-most one so they can be stacked and clear
-//   out until there are none.
-// </summary>
 // --------------------------------------------------------------------------------------------------------------------
 namespace TrailSimulation.Core
 {
@@ -20,8 +15,8 @@ namespace TrailSimulation.Core
     /// <summary>Facilitates the ability to control the entire simulation with the passes interface reference. Server simulation
     ///     keeps track of all currently loaded game modes and will only tick the top-most one so they can be stacked and clear
     ///     out until there are none.</summary>
-    /// <typeparam name="TCommands"></typeparam>
-    /// <typeparam name="TData"></typeparam>
+    /// <typeparam name="TCommands">Enumeration of all the available commands this window supports.</typeparam>
+    /// <typeparam name="TData">Window data class that will be used for this window.</typeparam>
     public abstract class Window<TCommands, TData> :
         Comparer<IWindow>, 
         IComparable<Window<TCommands, TData>>, 
@@ -32,10 +27,15 @@ namespace TrailSimulation.Core
         where TData : WindowData, new()
     {
         /// <summary>
+        ///     Keeps track of the total number of menu choices that are currently available to this window.
+        /// </summary>
+        private int _menuChoiceCount;
+
+        /// <summary>
         ///     Reference to all of the possible commands that this game Windows supports routing back to the game simulation that
         ///     spawned it.
         /// </summary>
-        private HashSet<IMenuChoice<TCommands>> _menuChoices;
+        private List<IMenuChoice<TCommands>> _menuCommands;
 
         /// <summary>
         ///     Holds the footer text that we will place below menu but before input buffer text.
@@ -48,15 +48,32 @@ namespace TrailSimulation.Core
         private string _menuHeader;
 
         /// <summary>
+        /// Reference for mappings to go from string data into a valid loaded input enumeration value.
+        /// </summary>
+        private Dictionary<string, TCommands> _menuMappings;
+
+        /// <summary>
+        /// Reference for mappings to go from enumeration value to action.
+        /// </summary>
+        private Dictionary<TCommands, Action> _menuActions;
+
+        /// <summary>
+        ///     Holds the text user interface data that we are going to eventually render out to the user.
+        /// </summary>
+        private StringBuilder _menuPrompt;
+
+        /// <summary>
         ///     Initializes a new instance of the <see cref="Window{TCommands,TData}" /> class.
-        ///     Initializes a new instance of the <see cref="T:TrailEntities.Window" /> class.
         /// </summary>
         protected Window()
         {
+            // Reference for our text user interface data so we can build it up in pieces.
+            _menuPrompt = new StringBuilder();
+
             // Create the user data object casted to correct type from generics while still adhering to common base class.
             UserData = TypeExtensions.New<TData>.Instance();
 
-            // Determines if the menu system should show raw command names in the menu rendering or just number selections by enum value.
+            // Determines if the menu system should show raw input names in the menu rendering or just number selections by enum value.
             ShowCommandNamesInMenu = SimulationApp.SHOW_COMMANDS;
 
             // Complain the generics implemented is not of an enum type.
@@ -66,7 +83,11 @@ namespace TrailSimulation.Core
             }
 
             // Create empty list of menu choices.
-            _menuChoices = new HashSet<IMenuChoice<TCommands>>();
+            _menuCommands = new List<IMenuChoice<TCommands>>();
+
+            // Dictionary of mappings for the choices and their associated values and actions.
+            _menuMappings = new Dictionary<string, TCommands>();
+            _menuActions = new Dictionary<TCommands, Action>();
 
             // Menu header and footer is empty strings by default.
             _menuHeader = string.Empty;
@@ -80,8 +101,7 @@ namespace TrailSimulation.Core
 
         /// <summary>
         ///     Defines the text prefix which will go above the menu, used to show any useful information the game Windows might
-        ///     need
-        ///     to at the top of menu selections.
+        ///     need to at the top of menu selections.
         /// </summary>
         protected virtual string MenuHeader
         {
@@ -100,7 +120,7 @@ namespace TrailSimulation.Core
         }
 
         /// <summary>
-        ///     Determines if the command names for the particular action should be printed out alongside the number the user can
+        ///     Determines if the input names for the particular action should be printed out alongside the number the user can
         ///     press to control that particular enum.
         /// </summary>
         private bool ShowCommandNamesInMenu { get; }
@@ -110,6 +130,31 @@ namespace TrailSimulation.Core
         ///     data for parent game Windows.
         /// </summary>
         protected TData UserData { get; }
+
+        /// <summary>
+        ///     Because of how generics work in C# we need to have the ability to override a method in implementing classes to get
+        ///     back the correct commands for the implementation from abstract class inheritance chain. On the bright side it
+        ///     enforces the commands returned to be of the specified enum in generics.
+        /// </summary>
+        /// <remarks>
+        ///     http://stackoverflow.com/a/5042675
+        /// </remarks>
+        /// <returns>
+        ///     Formatting list of enumeration values that can be iterated over as array.
+        /// </returns>
+        private static TCommands[] Commands
+        {
+            get
+            {
+                // Complain the generics implemented is not of an enum type.
+                if (!typeof (TCommands).IsEnum)
+                {
+                    throw new InvalidCastException("T must be an enumerated type!");
+                }
+
+                return Enum.GetValues(typeof (TCommands)) as TCommands[];
+            }
+        }
 
         /// <summary>The compare to.</summary>
         /// <param name="other">The other.</param>
@@ -193,7 +238,7 @@ namespace TrailSimulation.Core
             Form = null;
 
             // Allows any data structures that care about themselves to save before the next tick comes.
-            OnModeRemoved(WindowCategory);
+            OnModeRemoved();
         }
 
         /// <summary>
@@ -239,78 +284,65 @@ namespace TrailSimulation.Core
         ///     display to user console specific information about what the simulation wants.
         /// </summary>
         /// <returns>
-        ///     The <see cref="string" />.
+        ///     The windows text user interface<see cref="string" />.
         /// </returns>
-        public string OnRenderMode()
+        public string OnRenderWindow()
         {
-            // Build up string representation of the current state of game window.
-            var modeTUI = new StringBuilder();
-
             // Only add menu choices if there are some to actually add, otherwise just return string buffer now.
-            if (_menuChoices?.Count > 0 && Form == null)
+            _menuPrompt.Clear();
+            if (_menuCommands?.Count > 0 && Form == null)
             {
                 // Header text for above menu.
                 if (!string.IsNullOrEmpty(MenuHeader))
-                    modeTUI.Append($"{MenuHeader}{Environment.NewLine}{Environment.NewLine}");
+                    _menuPrompt.Append($"{MenuHeader}{Environment.NewLine}{Environment.NewLine}");
 
-                // Loop through the menu choices and add each one to text user interface.
-                var menuChoices = 1;
-                foreach (var menuChoice in _menuChoices)
-                {
-                    // Name of command and then description of what it does, the command is all we really care about.
-                    modeTUI.Append(ShowCommandNamesInMenu
-                        ? $"  {menuChoices}. {menuChoice.Command} - {menuChoice.Description}{Environment.NewLine}"
-                        : $"  {menuChoices}. {menuChoice.Description}{Environment.NewLine}");
-
-                    // Increment the menu choices number shown to user.
-                    menuChoices++;
-                }
+                // Figures out which input names and numbers goto what menu choice.
+                RefreshCommandMappings();
 
                 // Footer text for below menu.
                 if (!string.IsNullOrEmpty(MenuFooter))
-                    modeTUI.Append($"{MenuFooter}{Environment.NewLine}");
+                    _menuPrompt.Append($"{MenuFooter}{Environment.NewLine}");
             }
             else
             {
                 // Added any descriptive text about the Windows, like stats, health, weather, location, etc.
                 var prependMessage = Form?.OnRenderForm();
                 if (!string.IsNullOrEmpty(prependMessage))
-                    modeTUI.Append($"{prependMessage}{Environment.NewLine}");
+                    _menuPrompt.Append($"{prependMessage}{Environment.NewLine}");
             }
 
             // Returns the string buffer we constructed for this game Windows to the simulation so it can be displayed.
-            return modeTUI.ToString();
+            return _menuPrompt.ToString();
         }
 
-        /// <summary>Fired by messaging system or user interface that wants to interact with the simulation by sending string command
-        ///     that should be able to be parsed into a valid command that can be run on the current game Windows.</summary>
-        /// <param name="command">Passed in command from controller, text was trimmed but nothing more.</param>
-        public void SendCommand(string command)
+        /// <summary>Fired by messaging system or user interface that wants to interact with the simulation by sending string input
+        ///     that should be able to be parsed into a valid input that can be run on the current game Windows.</summary>
+        /// <param name="input">Passed in input from controller, text was trimmed but nothing more.</param>
+        public void SendCommand(string input)
         {
             // Only process menu items for game Windows when current state is null, or there are no menu choices to select from.
             if (Form == null &&
-                _menuChoices?.Count > 0 &&
-                !string.IsNullOrEmpty(command) &&
-                !string.IsNullOrWhiteSpace(command))
+                _menuCommands?.Count > 0 &&
+                !string.IsNullOrEmpty(input) &&
+                !string.IsNullOrWhiteSpace(input))
             {
-                // Loop through every added menu choice.
-                foreach (var menuChoice in _menuChoices)
-                {
-                    // Attempt to convert the returned line into generic enum.
-                    TCommands parsedCommandValue;
-                    Enum.TryParse(command, out parsedCommandValue);
-                    if (!(Enum.IsDefined(typeof (TCommands), parsedCommandValue) |
-                          parsedCommandValue.ToString(CultureInfo.InvariantCulture).Contains(",")))
-                        continue;
-
-                    // Check if the received input buffer matches any of them.
-                    if (!parsedCommandValue.Equals(menuChoice.Command))
-                        continue;
-
-                    // If it matches then invoke the bound action in the simulation.
-                    menuChoice.Action.Invoke();
+                // Attempt to convert the returned line into generic enum.
+                TCommands parsedCommand;
+                Enum.TryParse(input, out parsedCommand);
+                if (!(Enum.IsDefined(typeof (TCommands), parsedCommand) |
+                      parsedCommand.ToString(CultureInfo.InvariantCulture).Contains(",")))
                     return;
-                }
+
+                // Skip if the input does not match any known mapping for menu choices.
+                if (!_menuMappings.ContainsKey(input))
+                    return;
+
+                // Check if the choice exists in the dictionary of choices valid for this river crossing location.
+                if (!_menuActions.ContainsKey(_menuMappings[input]))
+                    return;
+
+                // Invoke the anonymous delegate method that was created when this form was attached.
+                _menuActions[_menuMappings[input]].Invoke();
             }
             else
             {
@@ -323,7 +355,7 @@ namespace TrailSimulation.Core
                     return;
 
                 // Pass the input buffer to the current state, if it manages to get this far.
-                Form.OnInputBufferReturned(command);
+                Form.OnInputBufferReturned(input);
             }
         }
 
@@ -429,47 +461,56 @@ namespace TrailSimulation.Core
             // Nothing to see here, move along...
         }
 
-        /// <summary>
-        ///     Because of how generics work in C# we need to have the ability to override a method in implementing classes to get
-        ///     back the correct commands for the implementation from abstract class inheritance chain. On the bright side it
-        ///     enforces the commands returned to be of the specified enum in generics.
-        /// </summary>
-        /// <remarks>
-        ///     http://stackoverflow.com/a/5042675
-        /// </remarks>
-        /// <returns>
-        ///     The <see cref="TCommands[]" />.
-        /// </returns>
-        private static TCommands[] GetCommands()
-        {
-            // Complain the generics implemented is not of an enum type.
-            if (!typeof (TCommands).IsEnum)
-            {
-                throw new InvalidCastException("T must be an enumerated type!");
-            }
-
-            return Enum.GetValues(typeof (TCommands)) as TCommands[];
-        }
-
-        /// <summary>Adds a new game Windows menu selection that will be available to send as a command for this specific game
+        /// <summary>Adds a new game Windows menu selection that will be available to send as a input for this specific game
         ///     Windows.</summary>
         /// <param name="action">Method that will be run when the choice is made.</param>
-        /// <param name="command">Associated command that will trigger the respective action in the active game Windows.</param>
+        /// <param name="command">Associated input that will trigger the respective action in the active game Windows.</param>
         /// <param name="description">Text that will be shown to user so they know what the choice means.</param>
-        protected void AddCommand(Action action, TCommands command, string description)
+        private void AddCommand(Action action, TCommands command, string description)
         {
+            // Check to make sure the menu choice does not already exist in out collection.
             var menuChoice = new MenuChoice<TCommands>(command, action, description);
-            if (!_menuChoices.Contains(menuChoice))
+            if (_menuCommands.Contains(menuChoice))
+                return;
+
+            // Adds the input to the list of possible choices.
+            _menuCommands.Add(menuChoice);
+        }
+
+        /// <summary>
+        ///     Recalculates all of the commands mappings giving them a clear path from a string input
+        /// </summary>
+        private void RefreshCommandMappings()
+        {
+            // Clear out any previous mappings.
+            _menuMappings.Clear();
+            _menuActions.Clear();
+            _menuChoiceCount = 0;
+
+            // Loop through every menu input we have.
+            foreach (var menuChoice in _menuCommands)
             {
-                _menuChoices.Add(menuChoice);
+                // Increment the total number of commands we have now.
+                _menuChoiceCount++;
+
+                // Add the input to the mapping dictionary, and the delegate for it's action invoker.
+                _menuMappings.Add(_menuChoiceCount.ToString(), menuChoice.Command);
+
+                // Adds the reverse mapping for actual enumeration value input to related action.
+                _menuActions.Add(menuChoice.Command, menuChoice.Action);
+
+                // Name of input and then description of what it does, the input is all we really care about.
+                _menuPrompt.Append(ShowCommandNamesInMenu
+                    ? $"  {_menuChoiceCount}. {menuChoice.Command} - {menuChoice.Description}{Environment.NewLine}"
+                    : $"  {_menuChoiceCount}. {menuChoice.Description}{Environment.NewLine}");
             }
         }
 
-        /// <summary>Adds a new game menu selection with description pulled from attribute on command enumeration. This override is not
+        /// <summary>Adds a new game menu selection with description pulled from attribute on input enumeration. This override is not
         ///     meant for menu selections where you want to manually specify the description of the menu item, this way it will be
         ///     pulled from enum description attribute.</summary>
         /// <param name="action">Method that will be run when the choice is made.</param>
-        /// <param name="command">Associated command that will trigger the respective action in the active game Windows.</param>
+        /// <param name="command">Associated input that will trigger the respective action in the active game Windows.</param>
         protected void AddCommand(Action action, TCommands command)
         {
             AddCommand(action, command, command.ToDescriptionAttribute());
@@ -481,14 +522,18 @@ namespace TrailSimulation.Core
         /// </summary>
         protected void ClearCommands()
         {
-            _menuChoices.Clear();
+            // Clears both the commands added by window on create, and the mapping list generated from this class.
+            _menuCommands.Clear();
+            _menuMappings.Clear();
+            _menuActions.Clear();
         }
 
         /// <summary>Fired when this game Windows is removed from the list of available and ticked modes in the simulation.</summary>
-        /// <param name="windows">The windows.</param>
-        protected virtual void OnModeRemoved(GameWindow windows)
+        protected virtual void OnModeRemoved()
         {
-            _menuChoices = null;
+            _menuCommands = null;
+            _menuMappings = null;
+            _menuActions = null;
         }
 
         /// <summary>
